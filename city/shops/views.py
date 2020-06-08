c71ad82aab2a51d5b79f9610a1e379b9220d1f86
KuzminN <kng197@mail.ru>
2020-06-08 14:23:20 +0400

    Работа с фильтрацией(не до конца)

--- city/shops/views.py
+++ city/shops/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
 from django.http import *
 from .models import City
+from .models import Street
+from .models import Shop
+import datetime
+from django.utils import timezone
+
 # Create your views here.
 
 def Getcities(request):
     cities = City.objects.all()
     return render(request, "index.html", {"cities": cities})
+
+
 def Getstreets(request, city):
-    output = "<h2>All streets of {0}</h2>".format(city)
-    return HttpResponse(output)
+    streets = Street.objects.filter(street_city_id=city)
+    return render(request, "streets.html", {"streets": streets})
+
 
 def Shopcreate(request):
     return HttpResponseBadRequest("<h2>Shop create</h2>")
 
+
 def Getshop(request):
-    street = request.GET.get("street", -1)
-    city = request.GET.get("shops", 1)
-    open = request.GET.get("open", 0)
-    output = "<h2>Street id:{0}  shops id:{1} open:{2}</h2>".format(street, city, open)
-    return HttpResponse(output)
+    street = request.GET.get("street")
+    city = request.GET.get("city")
+    open = request.GET.get("open")
+    shops = Shop.objects.filter(shop_city_id=city, shop_street_id=street, shop_time_to_open__lte=timezone.now(), shop_time_to_close__gte=timezone.now())
+    return render(request, "getshops.html", {"shops": shops})
+
+
